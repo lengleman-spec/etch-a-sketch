157350cea43be5cb59d0252b58c9e39bd8b3c173
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-07 15:11:19 -0500

    Add logic to wipe exisiting squares when 'clear' button is pressed.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.getElementById("container");
-const containerSize = 960;
+const containerSize = 960; //Used to calculate square size
+
+const button = document.createElement("button");
+button.textContent = "Clear";
+button.type = "button";
+document.body.prepend(button);
+
+function createGrid(squaresPerSide) {
+  container.innerHTML = ""; // Removes all exisiting squares
+}
 
 for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement("div");
@@ -14,7 +23,3 @@ for (let i = 0; i < 16 * 16; i++) {
 }
 // appendChild() is last because we want to modify it when it's still in memory and not rendered on the page yet
 // In the loop we 1. create, 2. configure/manipulate 3. append it to the parent. Now it will be rendered.
-
-const button = document.createElement("button");
-button.textContent = "Clear";
-button.type = "button";
