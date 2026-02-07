f4f45997c17482da41d47fd7b9d8877c0c8f6a88
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-07 15:03:56 -0500

    Add logic to create the divs and the clear button.

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+const containerSize = 960;
+
+for (let i = 0; i < 16 * 16; i++) {
+  const div = document.createElement("div");
+  div.classList.add("square");
+  //   container.appendChild(div);
+
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "green";
+  });
+
+  container.appendChild(div);
+}
+// appendChild() is last because we want to modify it when it's still in memory and not rendered on the page yet
+// In the loop we 1. create, 2. configure/manipulate 3. append it to the parent. Now it will be rendered.
+
+const button = document.createElement("button");
+button.textContent = "Clear";
+button.type = "button";
